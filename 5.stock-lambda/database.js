4015cf4e09075ba6fa62bb614fbc4f7b82211e60
glen15 <glen910115@gmail.com>
2023-06-19 20:54:02 +0900

    update func for db in stock-lambda

--- 5.stock-lambda/database.js
+++ 5.stock-lambda/database.js
@@ -13,20 +13,18 @@ const connectDb = async (req, res, next) => {
     req.conn = await mysql.createConnection({ host, user, password, database })
     next()
   }
-  catch(e) {
+  catch (e) {
     console.log(e)
     res.status(500).json({ message: "데이터베이스 연결 오류" })
   }
 }
 
 const getProduct = (sku) => `
-  SELECT BIN_TO_UUID(product_id) as product_id, name, price, stock, BIN_TO_UUID(factory_id), BIN_TO_UUID(ad_id)
-  FROM product
-  WHERE sku = "${sku}"
+  SELECT * FROM product WHERE sku = "${sku}"
 `
 
 const increaseStock = (productId, incremental) => `
-  UPDATE product SET stock = stock + ${incremental} WHERE product_id = UUID_TO_BIN('${productId}')
+  UPDATE product SET stock = stock + ${incremental} WHERE product_id = '${productId}'
 `
 
 module.exports = {
