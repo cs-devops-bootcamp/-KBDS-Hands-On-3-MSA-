40fee2deb2d9121fe6c07ea1b0912c648112441f
glen15 <glen910115@gmail.com>
2023-06-19 16:57:12 +0900

    update order-lambda for connect factory

--- 3.order-lambda/index.js
+++ 3.order-lambda/index.js
@@ -7,12 +7,12 @@ const consumer = async (event) => {
       "MessageAttributeProductId": json.MessageAttributeProductId.Value,
       "MessageAttributeProductCnt": json.MessageAttributeProductCnt.Value,
       "OrderStockCnt": 10,
-      "CallbackUrl": "https://9ckxfwlrda.execute-api.ap-northeast-2.amazonaws.com/product/donut"
+      "CallbackUrl": "your Stock-Lambda-api_gw URL"
     }
     console.log(`공장으로 보내는 메시지 페이로드 : ${JSON.stringify(payload)}`)
     try {
       const response = await axios.post(
-        `http://project3-factory.coz-devops.click/api/manufactures`,
+        `http://3.34.185.91/api/manufactures`,
         payload
       );
       console.log(`응답 정보 : ${response.data}`);
